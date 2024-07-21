7adf681e33ae7cf36b75070ba3314145007cc95c
Larisa <laurabrovkina@gmail.com>
2024-07-22 11:46:24 +1200

    Read file with numbers and them sum them up in parallel using pod lock

---
+++ ReadFileAndSum/Constants.cs
@@ -0,0 +1,7 @@
+namespace ReadFileAndSum;
+
+public static class Constants
+{
+    public static string LargeNumbers = "random_numbers_large.txt";
+    public static string Numbers = "random_numbers.txt";
+}
---
+++ ReadFileAndSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using ReadFileAndSum;
+using static System.Int32;
+
+var padLock = new object();
+
+using var reader = new StreamReader(Constants.LargeNumbers);
+var line = string.Empty;
+while ((line = reader.ReadLine()) is not null)
+{
+    var values = line.Split(',');
+    var sum = 0;
+
+    Parallel.ForEach(values, value =>
+    {
+        lock (padLock)
+        {
+            _ = TryParse(value, out var number);
+            sum += number;
+        }
+    });
+
+    Console.WriteLine("Sum of the numbers in a file");
+    Console.WriteLine(sum);
+}
